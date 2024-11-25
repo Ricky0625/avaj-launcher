5c470282cc6ed094aea5ab270e9b4be9f4bbd5d4
rickyyyy <rickywong0625@gmail.com>
2024-11-25 21:26:59 +0800

    refactor: restructure test folder

--- src/test/abstractions/TestRunner.java
+++ src/test/abstractions/TestRunner.java
@@ -6,8 +6,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import utils.LoggerUtils;
 import utils.TestCase;
+import utils.LoggerUtils;
 
 public abstract class TestRunner {
 
