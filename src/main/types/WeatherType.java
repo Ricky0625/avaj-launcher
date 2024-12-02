e6f0054a6bdf634edec657a1135676ea2e581ec3
rickyyyy <rickywong0625@gmail.com>
2024-12-02 22:14:17 +0800

    feat: add getWeathers and fromString for WeatherType

--- src/main/types/WeatherType.java
+++ src/main/types/WeatherType.java
@@ -10,4 +10,19 @@ public enum WeatherType {
     public String toString() {
         return name();
     }
+
+    public static String[] getWeathers() {
+        WeatherType[] types = WeatherType.values();
+        String[] weathers = new String[types.length];
+
+        for (int i = 0; i < types.length; i++) {
+            weathers[i] = types[i].name();
+        }
+
+        return weathers;
+    }
+
+    public static WeatherType fromString(String name) throws IllegalArgumentException {
+        return WeatherType.valueOf(name.toUpperCase());
+    }
 }
