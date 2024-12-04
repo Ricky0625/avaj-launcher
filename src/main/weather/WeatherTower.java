51c909009b592e18a9c158bd02364a4ede21d7fe
rickyyyy <rickywong0625@gmail.com>
2024-12-04 22:50:01 +0800

    feat: complete the implementation for WeatherTower

--- src/main/weather/WeatherTower.java
+++ src/main/weather/WeatherTower.java
@@ -5,12 +5,15 @@ import base.Tower;
 
 public class WeatherTower extends Tower {
 
+    public WeatherTower() {
+    }
+
     public String getWeather(Coordinates p_coordinates) {
         return WeatherProvider.getInstance().getCurrentWeather(p_coordinates);
     }
 
     public void changeWeather() {
-        ;
+        contitionChanged(); // from Tower class
     }
 
 }
